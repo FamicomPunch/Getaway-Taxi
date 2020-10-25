c7ffeb4f8612904fa7c5def46caf194ecf370841
Comp0sator <Quarterinchspam@yahoo.com>
2020-10-25 16:40:34 -0400

    Started Ramp Mechanic

---
+++ Assets/Wiktor/Scripts/Ramp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramp : MonoBehaviour
+{
+    public GameObject vehicleHolder;
+
+    private Animator anim;
+    private GameObject vehicle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void EndRamp()
+    {
+        vehicle.transform.SetParent(null);
+        vehicle.GetComponent<PlayerController>().enabled = true;
+        vehicle = null;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("ON RAMP");
+            vehicle = other.gameObject;
+            vehicle.GetComponent<PlayerController>().enabled = false;
+            vehicle.transform.SetParent(vehicleHolder.transform);
+            vehicle.transform.position = vehicleHolder.transform.position;
+            anim.SetTrigger("OnRamp");
+        }
+    }
+}
