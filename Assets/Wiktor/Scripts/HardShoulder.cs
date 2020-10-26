3d8a406d338e32e1fd5bb7fe674e57883bee7baf
Comp0sator <Quarterinchspam@yahoo.com>
2020-10-26 02:07:19 -0400

    Updated prefabs and added screen shake mechanic

---
+++ Assets/Wiktor/Scripts/HardShoulder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardShoulder : MonoBehaviour
+{
+    private ScreenShake shaker;
+
+    private void Start()
+    {
+        shaker = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ScreenShake>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //if(!shaker.onShoulder)
+            //{
+                shaker.onShoulder = true;
+                StartCoroutine(shaker.Shake(0.4f, 0.9f));
+            //}
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //shaker.EndShake();
+            //shaker.onShoulder = false;
+        }
+    }
+}
---
+++ Assets/Wiktor/Scripts/ScreenShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    [SerializeField] private float minRange = -0.1f;
+    [SerializeField] private float maxRange = 0.1f;
+
+    private Vector3 initialPos;
+    public bool onShoulder = false;
+
+    public IEnumerator Shake(float magnitude, float duration)
+    {
+        initialPos = transform.localPosition;
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float x = Random.Range(minRange, maxRange) * magnitude;
+            float y = Random.Range(minRange, maxRange) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, initialPos.z);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = initialPos;
+    }
+
+    public void EndShake()
+    {
+        transform.localPosition = initialPos;
+        StopCoroutine(Shake(0,0));
+    }
+}
